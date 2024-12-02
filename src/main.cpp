f0928ec210dfcdc494ca1053fee891bc8ebd47e7
Писарев Денис <d.pisarev20155@yandex.ru>
2024-12-02 11:08:39 +0300

    lan02 1

--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,54 @@
 #include "karel.h"
 
 
+void PickAllBeepers();
+void TurnRight();
 //==============================================================================
 /// Программа для робота Карела
 ///
 void KarelProgram() {
-/// @todo Добавить программу для Карела
+    bool is_even = false;
+    while(FrontIsClear()){
+        while(FrontIsClear()){
+            PickAllBeepers();
+            Move();
+        }
+        PickAllBeepers();
+        if(!is_even){
+            TurnLeft();
+            if(!FrontIsClear())
+                return;
+            Move();
+            TurnLeft();
+            
+        }
+        else{
+            TurnRight();
+            if(!FrontIsClear())
+                return;
+            Move();
+            TurnRight();
+        }
+        is_even = !is_even;
+        }
 
 }
 
+void PickAllBeepers(){
+    while (BeepersPresent())
+        PickBeeper();
+}
 
+void TurnRight(){
+    for(int i = 0; i < 3; i++){
+        TurnLeft();
+    }
+}
 //==============================================================================
 /// Стартовая функция
 ///
 int main() {
-    LoadWorld("worlds/collect_newspaper.w");
+    LoadWorld("worlds/CB_01.w");
     KarelProgram();
     Finish();
     return 0;
